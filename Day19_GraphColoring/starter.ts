a3fa462b796287ecf5b7a788f531b1a037bcb962
Andrew Bishop <was.bishop@icloud.com>
2025-06-19 13:06:25 -0700

    day19 sol

---
+++ Day19_GraphColoring/starter.ts
@@ -0,0 +1,22 @@
+/*
+In this challenge, you are a software developer for a logistics company that manages supply chain 
+operations across various cities. The company is trying to optimize its operations to reduce 
+delivery time by guiding supply trucks through these interconnected cities.
+
+To make it easier for the drivers, the company wants every two adjacent cities to have 
+different colors so that the driver doesn't get confused. Here comes your challenge, you have 
+to write a function 'validateCityAndColor' which takes a graph where each node in the graph 
+represents a city and an object representing color assignments to each city. The function should 
+return true if every two connected cities have different colors; otherwise false.
+
+You can assume that each city is connected to at least one other city.
+
+*/
+
+export function validateCityAndColor(graph: Map<string, string[]>, 
+	colors: Map<string, string>): boolean {
+	// TODO: implement code here
+
+	return false
+}
+

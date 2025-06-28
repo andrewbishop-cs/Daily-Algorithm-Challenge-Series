beea7969959102b3e104c1203690a03182079d46
Andrew Bishop <was.bishop@icloud.com>
2025-06-27 17:08:45 -0700

    cleanup

--- Day026_AutoComplete/solution.ts
+++ Day026_AutoComplete/solution.ts
@@ -1,20 +1,3 @@
-/**
-* No one likes typing more than they have to, so let's build an autocomplete feature.
-* You're tasked with creating a class, AutoCompleteSystem that will receive strings of
-* phrases, and be able to complete them given a prefix.
-*
-* Examples:
-* Adding "washing hands in hot soap", inputting "washing" would return
-* "washing hands in hot soap", inputting "hands in" would return nothing since the
-* auto complete only matches prefix.
-*
-* We'll simulate this in a LeetCode environment by utilizing HashMaps to create a weighted Trie.
-*
-* The prefix will always consist of alphabetic characters and blanks (i.e. a-z and spaces)
-* Fill each mehtod out below, with 'addPhrase' entering a phrase into the system, and
-* 'autoComplete' returning a list of auto completed phrases.
-*/
-
 class TrieNode {
     children: Map<string, TrieNode>
     isEndOfWord: boolean
